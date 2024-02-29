10997b5e92852b4405bcad6ce61b3565d854def9
DESKTOP-KML0PAI\User <alakayleh.aya@gmail.com>
2024-02-29 14:55:48 +0200

    Example-1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sum(5,6));
+            display();
+            Console.WriteLine(text("ali"));
+        }
+
+        static int sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void display()
+        {
+           for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("*");
+            }
+        }
+
+        static string text(string word)
+        {
+            if(word == "Mohammed")
+            {
+                return "Good job";
+            }else if(word == "Yarub")
+            {
+                return "Free palastine";
+            }
+            else
+            {
+                return "Hello";
+            }
+
+        }
+    }
+}
